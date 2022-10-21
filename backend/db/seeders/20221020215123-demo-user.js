2b68da74f8285d62b2c3f0c28c4aee8af497221b
Brandon Shin <brandonjshin98@gmail.com>
2022-10-21 15:29:34 -0700

    Refactored firstName and lastName for User sign up.

--- backend/db/seeders/20221020215123-demo-user.js
+++ backend/db/seeders/20221020215123-demo-user.js
@@ -17,17 +17,23 @@ module.exports = {
       {
         email: 'brandonjshin98@gmail.com',
         username: 'Brandon',
-        hashedPassword: bcrypt.hashSync('password')
+        hashedPassword: bcrypt.hashSync('password'),
+        firstName: 'Brandon',
+        lastName: 'Shin'
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password1')
+        hashedPassword: bcrypt.hashSync('password1'),
+        firstName: 'Bob',
+        lastName: 'Stacy'
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password2')
+        hashedPassword: bcrypt.hashSync('password2'),
+        firstName: 'Kathleen',
+        lastName: 'Liu'
       }
     ], {});
   },
