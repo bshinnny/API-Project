9614b13cb13d652436d500f8d3513c87d796f052
Brandon Shin <brandonjshin98@gmail.com>
2022-11-18 14:24:47 -0800

    Handled warnings.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
-import * as spotsActions from "./store/spots";
+// import * as spotsActions from "./store/spots";
 import Navigation from "./components/Navigation";
 import AllSpots from "./components/Spots/AllSpots";
 import SpotDetails from "./components/Spots/SpotDetails";
