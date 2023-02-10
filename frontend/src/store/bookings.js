e3b4c7e7a830aebafaf6ccb76fa25f85eab7ecff
Brandon Shin <brandonjshin98@gmail.com>
2023-02-09 17:20:02 -0800

    Bookings started.

--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -1,3 +1,34 @@
 import { csrfFetch } from './csrf';
 
+const GET_ALL_USER_BOOKINGS = 'bookings/GET_ALL_USER_BOOKINGS';
 
+// ACTIONS
+export const getAllUserBookings = (bookings) => {
+    return {
+        type: GET_ALL_USER_BOOKINGS,
+        bookings
+    }
+}
+
+// THUNKS
+export const getAllUserBookingsThunk = () => async dispatch => {
+    const response = await csrfFetch('/api/bookings/current')
+
+    if (response.ok) {
+        const userBookings = await response.json();
+        dispatch(getAllUserBookings(userBookings));
+        return response;
+    }
+}
+
+// Format initial data.
+const formatData = (array) => {
+    const object = {};
+    array.forEach(item => {
+        object[item.id] = item;
+    });
+    return object;
+}
+
+// REDUCER
+export default function bookingsReducer(state = )
