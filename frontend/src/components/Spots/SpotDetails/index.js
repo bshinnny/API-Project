38a3e5b1ffb80acbf271691aa423d2210ab40557
Brandon Shin <brandonjshin98@gmail.com>
2023-02-10 12:17:54 -0800

    Post booking completed.

--- frontend/src/components/Spots/SpotDetails/index.js
+++ frontend/src/components/Spots/SpotDetails/index.js
@@ -4,8 +4,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams, NavLink } from "react-router-dom";
 import * as spotsActions from '../../../store/spots';
 import * as reviewsActions from '../../../store/reviews';
+import * as bookingActions from '../../../store/bookings';
 import SpotReview from '../../Reviews/SpotReview';
 import './SpotDetails.css';
+import { useState } from 'react';
 
 function SpotDetails() {
     const dispatch = useDispatch()
@@ -15,6 +17,15 @@ function SpotDetails() {
     const reviews = useSelector(state => state.reviews.spotReviews);
     const user = useSelector(state => state.session.user);
 
+    const [checkIn, setCheckIn] = useState('TEST')
+    const [checkOut, setCheckOut] = useState(new Date())
+    const [guests, setGuests] = useState('1')
+    const [errors, setErrors] = useState('')
+
+    // const testDate = new Date();
+    // console.log(checkIn)
+
+
     useEffect(() => {
         dispatch(spotsActions.getSpotDetailsThunk(spotId))
     }, [dispatch, spotId])
@@ -22,6 +33,27 @@ function SpotDetails() {
     useEffect(() => {
         dispatch(reviewsActions.getSpotReviewsThunk(spotId))
     }, [dispatch, spotId])
+
+    const handleSubmit = async (e) => {
+        // e.preventDefault();
+
+        // const errors = [];
+
+        const bookingDates = {
+            checkIn,
+            checkOut
+        };
+
+        // console.log(bookingDates);
+
+        return dispatch(bookingActions.createBookingThunk(bookingDates, spotId))
+            .then((booking) => {
+                setCheckIn('')
+                setCheckOut('')
+                setErrors([])
+            })
+    }
+
 
     if (!spot) return null;
 
@@ -56,47 +88,128 @@ function SpotDetails() {
                     {spot.SpotImages[4] ? (<img className='bottom-right-image' alt={`spotImg-${spot.SpotImages[4].id}`} src={spot.SpotImages[4].url}></img>) : <p>No Image Available</p>}
                 </div>
             </div>
-            <div className='details-container'>
-                <div className='profile-info-div'>
-                    <div className='spot-info'>
-                        <h2 className='hosted-by'>{`Entire spot hosted by ${spot.Owner.firstName}.`}</h2>
-                        <div className='room-info-div'>
-                            <p className='room-info'>
-                                8 guests
-                            </p>
-                            <p className='divider'>·</p>
-                            <p className='room-info'>
-                                3 bedrooms
-                            </p>
-                            <p className='divider'>·</p>
-                            <p className='room-info'>
-                                6 beds
-                            </p>
-                            <p className='divider'>·</p>
-                            <p className='room-info'>
-                                4 baths
-                            </p>
+            <div className='details-booking-cont'>
+                <div className='details-container'>
+                    <div className='profile-info-div'>
+                        <div className='spot-info'>
+                            <h2 className='hosted-by'>{`Entire spot hosted by ${spot.Owner.firstName}.`}</h2>
+                            <div className='room-info-div'>
+                                <p className='room-info'>
+                                    8 guests
+                                </p>
+                                <p className='divider'>·</p>
+                                <p className='room-info'>
+                                    3 bedrooms
+                                </p>
+                                <p className='divider'>·</p>
+                                <p className='room-info'>
+                                    6 beds
+                                </p>
+                                <p className='divider'>·</p>
+                                <p className='room-info'>
+                                    4 baths
+                                </p>
+                            </div>
+                        </div>
+                        <div className='profile-pic'>
+                            <i className="fa-solid fa-address-card fa-2x"></i>
                         </div>
                     </div>
-                    <div className='profile-pic'>
-                        <i className="fa-solid fa-address-card fa-2x"></i>
+                    <ul className='check-in'>
+                        <li>
+                            <h3 className='check-in-header'>{`${spot.Owner.firstName} is a Superhost.`}</h3>
+                            <p className='check-in-detail'>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</p>
+                        </li>
+                        <li>
+                            <h3 className='check-in-header'>Great check-in experience!</h3>
+                            <p className='check-in-detail'>100% of recent guests gave the check-in process a 5-star rating.</p>
+                        </li>
+                        <li>
+                            <h3 className='check-in-header-last'>Free cancellation for 48 hours.</h3>
+                        </li>
+                    </ul>
+                    <div className='spot-details-description'>
+                        <p className='spot-details-description-text'>{`${spot.description} Book now for an enjoyable stay in this spacious and aesthetically pleasing house.`}</p>
                     </div>
                 </div>
-                <ul className='check-in'>
-                    <li>
-                        <h3 className='check-in-header'>{`${spot.Owner.firstName} is a Superhost.`}</h3>
-                        <p className='check-in-detail'>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</p>
-                    </li>
-                    <li>
-                        <h3 className='check-in-header'>Great check-in experience!</h3>
-                        <p className='check-in-detail'>100% of recent guests gave the check-in process a 5-star rating.</p>
-                    </li>
-                    <li>
-                        <h3 className='check-in-header-last'>Free cancellation for 48 hours.</h3>
-                    </li>
-                </ul>
-                <div className='spot-details-description'>
-                    <p className='spot-details-description-text'>{`${spot.description} Book now for an enjoyable stay in this spacious and aesthetically pleasing house.`}</p>
+                <div className='bookings-container'>
+                    <div className='booking-form-div'>
+                        <div className='booking-header'>
+                            <div className='price-cont-bookings'>
+                                <div className='nightly-price'>{`$${spot.price}`}</div>
+                                <div className='night-text'>a night.</div>
+                            </div>
+                            <div className='bookings-reviews'>
+                                <p className='avg-rating'>
+                                    <i className="fa-solid fa-star"></i>{Number(spot.numReviews) < 1 ? `New`:`${Number(spot.avgStarRating).toFixed(1)}`}
+                                </p>
+                                <p className='divider'>·</p>
+                                <p className='review-header-info'>
+                                    {Number(spot.numReviews) === 0 || Number(spot.numReviews) > 1 ?`${Number(spot.numReviews)} reviews` : `${Number(spot.numReviews)} review`}
+                                </p>
+                            </div>
+                        </div>
+                        <div className='booking-input-div'>
+                            <form className='booking-form'>
+                                <div className='booking-form-input-div'>
+                                    <div className='checkin-checkout-div'>
+                                        <div className='booking-form-text'>CHECK-IN</div>
+                                        <input
+                                            className='booking-input'
+                                            type='date'
+                                            value={checkIn}
+                                            onChange={(e) => setCheckIn(e.target.value)}
+                                            required
+                                        />
+                                    </div>
+                                    <div className='checkin-checkout-div co'>
+                                        <div className='booking-form-text'>CHECK-OUT</div>
+                                        <input
+                                            className='booking-input'
+                                            type='date'
+                                            value={checkOut}
+                                            onChange={(e) => setCheckOut(e.target.value)}
+                                            required
+                                        />
+                                    </div>
+                                </div>
+                                <div className='guests-div'>
+                                    <div className='booking-form-text'>GUESTS</div>
+                                    <select
+                                        className='booking-input-guests'
+                                        type='number'
+                                        value={guests}
+                                        onChange={(e) => setGuests(e.target.value)}
+                                        required
+                                    >
+                                        <option value='1'>1 guest</option>
+                                        <option value='2'>2 guests</option>
+                                        <option value='3'>3 guests</option>
+                                        <option value='4'>4 guests</option>
+                                        <option value='5'>5 guests</option>
+                                        <option value='6'>6 guests</option>
+                                        <option value='7'>7 guests</option>
+                                        <option value='8'>8 guests</option>
+                                    </select>
+                                </div>
+                            </form>
+                        </div>
+                        <div>
+                            {errors.length > 0 && (
+                                <div className='errors'>
+                                    {errors.map((error) => (
+                                        <div key={error}>{error}</div>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+                        <button
+                            className="submit-button clickable reserve"
+                            onClick={() => handleSubmit()}
+                        >
+                            RESERVE
+                        </button>
+                    </div>
                 </div>
             </div>
             <div className='spot-details-reviews'>
@@ -105,17 +218,20 @@ function SpotDetails() {
                     <p className='divider'>·</p>
                     {Number(spot.numReviews) === 0 || Number(spot.numReviews) > 1 ?`${Number(spot.numReviews)} reviews!` : `${Number(spot.numReviews)} review!`}
                 </h2>
+                <div className='create-review-div'>
+                    {user && spot.Owner.id !== user.id && (<NavLink className='nav-link create-review' to={`/spots/${spotId}/reviews/new`}>Create A Review</NavLink>)}
+                </div>
             </div>
             <div className='reviews-container'>
                 {reviews && Object.values(reviews).length > 0 ? Object.values(reviews).map((review) => (
                     <SpotReview key={`review-${review.id}`} review={review} spot={spot} />
                 )) :
-                <h2 className='reviews-h2'>Spot doesn't have any reviews. Be the first below!</h2>
+                <h2 className='reviews-h2'>Spot doesn't have any reviews. Be the first!</h2>
                 }
             </div>
-            <div className='create-review-div'>
+            {/* <div className='create-review-div'>
                 {user && spot.Owner.id !== user.id && (<NavLink className='nav-link create-review' to={`/spots/${spotId}/reviews/new`}>Create A Review</NavLink>)}
-            </div>
+            </div> */}
         </div>
     )
 }
--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -1,12 +1,20 @@
 import { csrfFetch } from './csrf';
 
 const GET_ALL_USER_BOOKINGS = 'bookings/GET_ALL_USER_BOOKINGS';
+const CREATE_BOOKING = 'bookings/CREATE_BOOKING';
 
 // ACTIONS
 export const getAllUserBookings = (bookings) => {
     return {
         type: GET_ALL_USER_BOOKINGS,
         bookings
+    }
+}
+
+export const createBooking = (booking) => {
+    return {
+        type: CREATE_BOOKING,
+        booking
     }
 }
 
@@ -21,6 +29,28 @@ export const getAllUserBookingsThunk = () => async dispatch => {
     }
 }
 
+export const createBookingThunk = (dates, spotId) => async dispatch => {
+    const {checkIn, checkOut} = dates;
+    const startDate = checkIn;
+    const endDate = checkOut;
+
+    // console.log(JSON.stringify({startDate, endDate}))
+
+    const response = await csrfFetch(`/api/spots/${spotId}/bookings`, {
+        method: 'POST',
+        body: JSON.stringify({
+            startDate,
+            endDate
+        })
+    })
+
+    if (response.ok) {
+        const booking = await response.json();
+        dispatch(createBooking(booking));
+        return booking;
+    }
+}
+
 // Format initial data.
 const formatData = (array) => {
     const object = {};
@@ -31,4 +61,23 @@ const formatData = (array) => {
 }
 
 // REDUCER
-export default function bookingsReducer(state = )
+const initalState = {
+    userBookings: {}
+}
+
+export default function bookingsReducer(state = initalState, action) {
+    let newState = {...state}
+    switch (action.type) {
+        case GET_ALL_USER_BOOKINGS:
+            const bookingsArr = action.bookings.Bookings;
+            const bookingsObj = formatData(bookingsArr);
+            newState = {...state, userBookings: bookingsObj};
+            return newState;
+        case CREATE_BOOKING:
+            newState = {...state, userBookings: {...state.userBookings}};
+            newState.userBookings[action.booking.id] = action.booking;
+            return newState
+        default:
+            return state;
+    }
+}
